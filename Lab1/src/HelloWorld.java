b52c5357ba4c6365dce3b7db8f7c98f7a7cf2dd4
staif <tailuffy4@gmail.com>
2026-03-27 16:56:19 +0700

    Complete assignment 2.2.1

--- Lab1/src/HelloWorld.java
+++ Lab1/src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello hihii!");
+        System.out.println("Xin chao \n cac ban!");
+        System.out.println("Hello \n world!");
     }
 }
