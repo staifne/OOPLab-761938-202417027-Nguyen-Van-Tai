fa745d6577c37302b60f36ec07116b3974099bf7
staif <tailuffy4@gmail.com>
2026-03-28 13:18:46 +0700

    Complete Exercises 6.6

---
+++ Lab1/src/MatrixAddition.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class MatrixAddition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+        int[][] matrix1 = new int[rows][cols];
+        int[][] matrix2 = new int[rows][cols];
+        int[][] sumMatrix = new int[rows][cols];
+        System.out.println("Enter elements for the first matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.printf("Matrix1[%d][%d] = ", i, j);
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Enter elements for the second matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.printf("Matrix2[%d][%d] = ", i, j);
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        System.out.println("\nResulting Matrix (Sum):");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(sumMatrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
