a81175c5924874f46a4f5ab4b1678780076e7de0
staif <tailuffy4@gmail.com>
2026-04-04 13:37:29 +0700

    Feature: Add TestPassingParameter to demonstrate pass-by-value and write a correct swap method.

--- Lab02/AimsProject/src/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -29,6 +29,9 @@ public class DigitalVideoDisc {
         this.length = length;
         this.cost = cost;
     }
+    public void setTitle(String title){
+        this.title = title;
+    }
     public String getTitle() { return title; }
     public String getCategory() { return category; }
     public String getDirector() { return director; }
