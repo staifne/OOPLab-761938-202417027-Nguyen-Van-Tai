f621535644382609a8e43755706da04be3014a61
staif <tailuffy4@gmail.com>
2026-04-04 14:12:38 +0700

    Feature: Add static counter and auto-increment ID for DigitalVideoDisc.

--- Lab02/AimsProject/src/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -4,15 +4,21 @@ public class DigitalVideoDisc {
     private String director;
     private int length;
     private float cost;
+    private static int nbDigitalVideoDiscs = 0;
+    private int id;
     public DigitalVideoDisc(String title){
         super();
         this.title = title;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String category, String title,float cost){
         super();
         this.category = category;
         this.title = title;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String director,String category,String title,float cost){
         super();
@@ -20,6 +26,8 @@ public class DigitalVideoDisc {
         this.category = category;
         this.title = title;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String title,String category,String director,int length,float cost){
         super();
@@ -28,9 +36,14 @@ public class DigitalVideoDisc {
         this.director = director;
         this.length = length;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public void setTitle(String title){
         this.title = title;
+    }
+    public int getId(){
+        return id;
     }
     public String getTitle() { return title; }
     public String getCategory() { return category; }
